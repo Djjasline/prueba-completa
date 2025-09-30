77cca74ee04719b132e33cf186b9a34e2966e84a
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-09-30 15:48:53 -0500

    Update listReportsByUser.js

--- netlify/functions/listReportsByUser.js
+++ netlify/functions/listReportsByUser.js
@@ -1,1 +1,25 @@
+const { MongoClient, ObjectId } = require("mongodb");
 
+const client = new MongoClient(process.env.MONGODB_URI);
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== "GET") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  try {
+    const userId = event.queryStringParameters.userId;
+    await client.connect();
+    const db = client.db("astap_reports");
+    const reports = db.collection("reports");
+
+    const data = await reports.find({ userId }).toArray();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.message };
+  }
+};
