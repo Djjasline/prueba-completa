f72882baf06116c86a86bebfcdb8f301eeb58385
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-11-25 10:01:00 -0500

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+
 import ScrollToTop from "./components/ScrollToTop";
 import ErrorBoundary from "./components/ErrorBoundary";
 import NotFound from "./pages/NotFound";
