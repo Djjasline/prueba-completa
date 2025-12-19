0fa881a2deb000e7501d4caad518cd78e2177756
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-19 12:12:29 -0500

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,52 +1,30 @@
-// src/Routes.jsx
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import ErrorBoundary from "./components/ErrorBoundary";
 import NotFound from "./pages/NotFound";
 
-import ServiceReportCreation from "./pages/service-report-creation";
-import EmailIntegrationInterface from "./pages/email-integration-interface";
-import DigitalSignatureCapture from "./pages/digital-signature-capture";
-import PDFReportPreview from "./pages/pdf-report-preview";
 import ReportHistoryManagement from "./pages/report-history-management";
 
-const Routes = () => {
+// 🔹 INSPECCIONES
+import InspeccionRoutes from "./app/inspeccion/Routes";
+
+export default function AppRoutes() {
   return (
     <BrowserRouter>
       <ErrorBoundary>
         <ScrollToTop />
-        <RouterRoutes>
-          {/* ✅ Pantalla inicial: listado de informes */}
+        <Routes>
+          {/* HOME */}
           <Route path="/" element={<ReportHistoryManagement />} />
 
-          {/* Alias extra por si hay enlaces viejos */}
-          <Route
-            path="/report-history-management"
-            element={<ReportHistoryManagement />}
-          />
-
-          {/* Flujo del informe */}
-          <Route
-            path="/service-report-creation"
-            element={<ServiceReportCreation />}
-          />
-          <Route
-            path="/digital-signature-capture"
-            element={<DigitalSignatureCapture />}
-          />
-          <Route path="/pdf-report-preview" element={<PDFReportPreview />} />
-          <Route
-            path="/email-integration-interface"
-            element={<EmailIntegrationInterface />}
-          />
+          {/* MÓDULO INSPECCIONES */}
+          <Route path="/inspeccion/*" element={<InspeccionRoutes />} />
 
           {/* 404 */}
           <Route path="*" element={<NotFound />} />
-        </RouterRoutes>
+        </Routes>
       </ErrorBoundary>
     </BrowserRouter>
   );
-};
-
-export default Routes;
+}
