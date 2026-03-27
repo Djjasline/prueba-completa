f5bb0ec9f0ad8f61347b7c51b43c7e83643ce56e
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-03-27 11:55:28 -0500

    Create user.js

---
+++ src/data/user.js
@@ -0,0 +1,12 @@
+export const USERS = [
+  // ADMIN
+  { email: "kamhez@astap.com", password: "Kamhez2908*", role: "admin" },
+  { email: "smaviles@astap.com", password: "1234", role: "admin" },
+  { email: "enieto@astap.com", password: "1717702318*", role: "admin" },
+
+  // TECNICOS
+  { email: "abriones@astap.com", password: "Jordan.72*", role: "tecnico" },
+  { email: "ppaucar@astap.com", password: "171719121*", role: "tecnico" },
+  { email: "kleon@astap.com", password: "172340310*", role: "tecnico" },
+  { email: "jpillajo@astap.com", password: "171894484*", role: "tecnico" },
+];
